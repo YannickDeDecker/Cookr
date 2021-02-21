34e5f6feee05731bae63ef59bbc0a005b2500f0a
Yannick De Decker <71879638+YannickDeDecker@users.noreply.github.com>
2021-02-21 14:05:40 +0100

    update router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
 } from "react-router-dom";
 import Home from './Pages/Home'
@@ -13,7 +13,7 @@ import Article from './Pages/Article'
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div>
           <Navbar/>
         </div>
